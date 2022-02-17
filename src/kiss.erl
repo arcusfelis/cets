6419fc6fe73b6ac6d32a6e67d25c646892319da0
Mikhail Uvarov <arcusfelis@gmail.com>
2022-02-17 15:45:45 +0100

    Working

--- src/kiss.erl
+++ src/kiss.erl
@@ -6,59 +6,74 @@
 
 %% We don't use monitors to avoid round-trips (that's why we don't use calls neither)
 -module(kiss).
--export([start/2]).
+-export([start/2, dump/1, insert/2, join/2]).
+
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
-
--export([load_test/0]).
 
 -behaviour(gen_server).
 
 %% Table and server has the same name
 start(Tab, Opts) when is_atom(Tab) ->
-    ets:new(Tab, [ordered_set, named_table,
-                     public, {read_concurrency, true}]),
-    N = erlang:system_info(schedulers),
-    Ns = lists:seq(1, N),
-    Names = [list_to_atom(atom_to_list(Tab) ++ integer_to_list(NN)) || NN <- Ns],
-    persistent_term:put(Tab, list_to_tuple(Names)),
-    [gen_server:start({local, Name}, ?MODULE, [Tab], [])
-     || Name <- Names].
-
+    gen_server:start({local, Tab}, ?MODULE, [Tab], []).
 
 stop(Tab) ->
     gen_server:stop(Tab).
 
-load_test() ->
-    start(tab1, #{}),
-    timer:tc(fun() ->
-            pmap(fun(X) -> load_test2(1000000) end, lists:seq(1, erlang:system_info(schedulers)))
-        end).
+dump(Tab) ->
+    ets:tab2list(Tab).
 
-load_test2(0) -> ok;
-load_test2(N) ->
-    S = erlang:system_info(scheduler_id),
-    Names = persistent_term:get(tab1),
-    Name = element(S, Names),
-    gen_server:call(Name, {insert, {N}}),
-    load_test2(N - 1).
+join(RemoteNode, Tab) when RemoteNode =/= node() ->
+    gen_server:call(Tab, {join, RemoteNode}, infinity).
 
-pmap(F, List) ->
-   Parent = self(),
-   Pids = [spawn_link(fun() -> Parent ! {result, self(), (catch F(X))} end) || X <- List],
-   [receive {result, P, Res} when Pid =:= P -> Res end
-    || Pid <- Pids].
+remote_add_node_to_schema(RemotePid, ServerPid, OtherNodes) ->
+    gen_server:call(RemotePid, {remote_add_node_to_schema, ServerPid, OtherNodes}, infinity).
+
+send_dump_to_remote_node(RemotePid, FromPid, OurDump) ->
+    gen_server:call(RemotePid, {send_dump_to_remote_node, FromPid, OurDump}, infinity).
 
 %% Key is {USR, Sid, UpdateNumber}
 %% Where first UpdateNumber is 0
 insert(Tab, Rec) ->
-%   Nodes = other_nodes(Tab),
-    ets:insert_new(Tab, Rec).
+    Nodes = other_nodes(Tab),
+    ets:insert_new(Tab, Rec),
     %% Insert to other nodes and block till written
+    Monitors = insert_to_remote_nodes(Nodes, Rec),
+    wait_for_inserted(Monitors).
+
+insert_to_remote_nodes([{RemoteNode, RemotePid, ProxyPid}|Nodes], Rec) ->
+    Mon = erlang:monitor(process, ProxyPid),
+    erlang:send(RemotePid, {insert_from_remote_node, Mon, self(), Rec}, [noconnect]),
+    [Mon|insert_to_remote_nodes(Nodes, Rec)];
+insert_to_remote_nodes([], _Rec) ->
+    [].
+
+wait_for_inserted([Mon|Monitors]) ->
+    receive
+        {inserted, Mon2} when Mon2 =:= Mon ->
+            wait_for_inserted(Monitors);
+        {'DOWN', Mon2, process, _Pid, _Reason} when Mon2 =:= Mon ->
+            wait_for_inserted(Monitors)
+    end;
+wait_for_inserted([]) ->
+    ok.
+
+other_nodes(Tab) ->
+    gen_server:call(Tab, get_other_nodes).
 
 init([Tab]) ->
-    {ok, #{tab => Tab}}.
+    ets:new(Tab, [ordered_set, named_table,
+                  public, {read_concurrency, true}]),
+    {ok, #{tab => Tab, other_nodes => []}}.
 
+handle_call({join, RemoteNode}, From, State) ->
+    handle_join(RemoteNode, State);
+handle_call({remote_add_node_to_schema, ServerPid, OtherNodes}, _From, State) ->
+    handle_remote_add_node_to_schema(ServerPid, OtherNodes, State);
+handle_call({send_dump_to_remote_node, FromPid, Dump}, _From, State) ->
+    handle_send_dump_to_remote_node(FromPid, Dump, State);
+handle_call(get_other_nodes, _From, State = #{other_nodes := Nodes}) ->
+    {reply, Nodes, State};
 handle_call({insert, Rec}, From, State = #{tab := Tab}) ->
     ets:insert_new(Tab, Rec),
     {reply, ok, State}.
@@ -66,7 +81,11 @@ handle_call({insert, Rec}, From, State = #{tab := Tab}) ->
 handle_cast(Msg, State) ->
     {noreply, State}.
 
-handle_info(Info, State) ->
+handle_info({'DOWN', _Mon, Pid, _Reason}, State) ->
+    handle_remote_down(Pid, State);
+handle_info({insert_from_remote_node, Mon, Pid, Rec}, State = #{tab := Tab}) ->
+    ets:insert_new(Tab, Rec),
+    Pid ! {inserted, Mon},
     {noreply, State}.
 
 terminate(_Reason, _State) ->
@@ -75,3 +94,67 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
+
+handle_join(RemoteNode, State = #{tab := Tab, other_nodes := Nodes}) ->
+    case lists:keymember(RemoteNode, 1, Nodes) of
+        true ->
+            %% Already added
+            {reply, ok, State};
+        false ->
+            RemotePid = rpc:call(RemoteNode, erlang, whereis, [Tab]),
+            case is_pid(RemotePid) of
+                false ->
+                    {reply, {error, remote_process_not_found}, State};
+                true ->
+                    case remote_add_node_to_schema(RemotePid, self(), Nodes) of
+                        {ok, Dump, OtherNodes} ->
+                            Mon = erlang:monitor(process, RemotePid),
+                            OtherPids = [Pid || {_, Pid, _} <- OtherNodes],
+                            Nodes2 = start_proxies_for([RemotePid|OtherPids]) ++ Nodes,
+                            OurDump = dump(Tab),
+                            send_dump_to_remote_node(RemotePid, self(), OurDump),
+                            insert_many(Tab, Dump),
+                            %% Add ourself into remote schema
+                            %% Add remote nodes into our schema
+                            %% Copy from our node / Copy into our node
+                            {reply, ok, State#{other_nodes => Nodes2}};
+                       Other ->
+                            error_logger:error_msg("remote_add_node_to_schema failed ~p", [Other]),
+                            {reply, {error, remote_add_node_to_schema_failed}, State}
+                    end
+            end
+    end.
+
+handle_remote_add_node_to_schema(ServerPid, OtherNodes, State = #{tab := Tab, other_nodes := Nodes}) ->
+    RemoteNode = node(ServerPid),
+    case lists:member(RemoteNode, Nodes) of
+        true ->
+            %% Already added (should not happen)
+            {reply, {error, already_added}, State};
+        false ->
+            Mon = erlang:monitor(process, ServerPid),
+            OtherPids = [Pid || {_, Pid, _} <- OtherNodes],
+            Nodes2 = start_proxies_for([ServerPid|OtherPids]) ++ Nodes,
+            {reply, {ok, dump(Tab), Nodes}, State#{other_nodes => Nodes2}}
+    end.
+
+start_proxies_for([RemotePid|OtherPids]) ->
+    RemoteNode = node(RemotePid),
+    {ok, ProxyPid} = kiss_proxy:start(RemotePid),
+    [{RemoteNode, RemotePid, ProxyPid}|start_proxies_for(OtherPids)];
+start_proxies_for([]) ->
+    [].
+
+handle_send_dump_to_remote_node(_FromPid, Dump, State = #{tab := Tab}) ->
+    insert_many(Tab, Dump),
+    {reply, ok, State}.
+
+insert_many(Tab, [Rec|Recs]) ->
+    ets:insert_new(Tab, Rec),
+    insert_many(Tab, Recs);
+insert_many(_Tab, []) ->
+    ok.
+
+handle_remote_down(Pid, State = #{other_nodes := Nodes}) ->
+    Nodes2 = lists:keydelete(Pid, 2, Nodes),
+    {noreply, State#{other_nodes => Nodes2}}.
--- test/kiss_SUITE.erl
+++ test/kiss_SUITE.erl
@@ -6,8 +6,10 @@
 all() -> [ets_tests].
  
 init_per_suite(Config) ->
-    {ok, Node2} = ct_slave:start(ct2, [{monitor_master, true}]),
-    [{node2, Node2}|Config].
+    Node2 = start_node(ct2),
+    Node3 = start_node(ct3),
+    Node4 = start_node(ct4),
+    [{nodes, [Node2, Node3, Node4]}|Config].
 
 end_per_suite(Config) ->
     Config.
@@ -19,7 +21,53 @@ end_per_testcase(ets_tests, Config) ->
     ok.
  
 ets_tests(Config) ->
-    Node2 = proplists:get_value(node2, Config),
-    kiss:start(tab1, #{}),
-    rpc:call(Node2, kiss, start, [tab1, #{}]),
+    Node1 = node(),
+    [Node2, Node3, Node4] = proplists:get_value(nodes, Config),
+    Tab = tab1,
+    start(Node1, Tab),
+    start(Node2, Tab),
+    start(Node3, Tab),
+    start(Node4, Tab),
+    join(Node1, Node3, Tab),
+    join(Node2, Node4, Tab),
+    insert(Node1, Tab, {a}),
+    insert(Node2, Tab, {b}),
+    insert(Node3, Tab, {c}),
+    insert(Node4, Tab, {d}),
+    ct:pal("n1 ~p~n", [dump(Node1, Tab)]),
+    ct:pal("n2 ~p~n", [dump(Node2, Tab)]),
+    join(Node1, Node2, Tab),
+    ct:pal("n1 ~p~n", [dump(Node1, Tab)]),
+    ct:pal("n2 ~p~n", [dump(Node2, Tab)]),
+    insert(Node1, Tab, {f}),
+    insert(Node4, Tab, {e}),
+%   [insert(Node1, Tab, {X}) || X <- lists:seq(1, 100000)],
+    ct:pal("n1 ~p~n", [dump(Node1, Tab)]),
+    ct:pal("n2 ~p~n", [dump(Node2, Tab)]),
+    error(oops),
     ok.
+
+start(Node, Tab) ->
+    rpc(Node, kiss, start, [Tab, #{}]).
+
+insert(Node, Tab, Rec) ->
+    rpc(Node, kiss, insert, [Tab, Rec]).
+
+dump(Node, Tab) ->
+    rpc(Node, kiss, dump, [Tab]).
+
+join(Node1, Node2, Tab) ->
+    rpc(Node1, kiss, join, [Node2, Tab]).
+
+rpc(Node, M, F, Args) ->
+    case rpc:call(Node, M, F, Args) of
+        {badrpc, Error} ->
+            ct:fail({badrpc, Error});
+        Other ->
+            Other
+    end.
+
+start_node(Sname) ->
+    {ok, Node} = ct_slave:start(Sname, [{monitor_master, true}]),
+    rpc:call(Node, code, add_paths, [code:get_path()]),
+    Node.
